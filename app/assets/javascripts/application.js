47613c68539b021d1f76a7c75dedeea39a8939b2
Danielid Ramirez <danielid132@gmail.com>
2022-02-08 21:14:25 -0300

    add jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery/dist/jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 //= require popper
 //= require bootstrap
-//= require rails-ujs
+
+
